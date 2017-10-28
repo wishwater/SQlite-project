620d6b8a1b17ef8583c00f29bfca759dac4f774d
wishwater <NikitaOgranchuck@gmail.com>
2017-10-28 11:31:19 +0300

    its work

---
+++ goiteens-master/models/__init__.py
@@ -0,0 +1,2 @@
+import sqlite3
+conn = sqlite3.connect('my_db.sqlite3', check_same_thread=False)
--- goiteens-master/models/executeSqlite3.py
+++ goiteens-master/models/executeSqlite3.py
@@ -1,33 +1,30 @@
 # -*- coding:utf-8 -*-
 
-import sqlite3
-
+from sqlite3 import IntegrityError
+from models import conn
 
 def executeSelectOne(sql):
-    conn = sqlite3.connect('my_db.sqlite3')
+
     curs = conn.cursor()
     curs.execute(sql)
     data = curs.fetchone()
-    conn.close()
+
     return data
 
 def executeSelectAll(sql):
-    conn = sqlite3.connect('my_db.sqlite3')
+
     curs = conn.cursor()
     curs.execute(sql)
     data = curs.fetchall()
-    conn.close()
+
     return data
 
 def executeSQL(sql):
-    conn = sqlite3.connect('my_db.sqlite3')
     try:
         curs = conn.cursor()
         curs.execute(sql)
         conn.commit()
-        conn.close()
         return True
-    except sqlite3.IntegrityError:
-        conn.close()
+    except IntegrityError:
         return False
 
